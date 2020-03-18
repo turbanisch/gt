dace0fc82f03ffb1831e36982e286a78aeaced04
Richard Iannone <riannone@me.com>
2020-03-17 20:41:15 -0400

    Make necessary changes for using roxygenlabs

--- R/gt-package.R
+++ R/gt-package.R
@@ -1,3 +1,7 @@
-#' @docType package
-#' @aliases gt-package
+#' A package for making display tables
+#'
+#' Use a high-level, declarative interface for making display tables.
+#'
+#' @theme assets/extra.css assets/rd.js
+#' @name gt-package
 "_PACKAGE"
